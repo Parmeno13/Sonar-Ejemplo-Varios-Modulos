9c10b918c7b9b06a76454196e53d2931404d5064
Parmeno13 <51698634+Parmeno13@users.noreply.github.com>
2019-06-13 12:49:56 +0200

    Update Marshall.java

--- JavaProject/src/tools/Marshall.java
+++ JavaProject/src/tools/Marshall.java
@@ -1,5 +1,19 @@
 package tools;
+import java.io.*
 
 public class Marshall {
-
+  Process theProcess = null;
+    BufferedReader inStream = null;
+ 
+        
+      try
+      {
+          theProcess = Runtime.getRuntime().exec("calc.exe");
+      }
+      catch(IOException e)
+      {
+         System.err.println("Error en el método exec()");
+         e.printStackTrace();  
+      }
+    
 }
